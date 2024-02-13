b415465ae17d08a225a5d5651a07dafa9cd1dacb
Stern_Ritter <fadeef.k@yandex.ru>
2024-02-13 13:32:12 +0300

    Init commit

---
+++ OOP/notebook/src/main/java/ru/otus/solid/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.solid;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Bob");
+        user.report();
+        user.save();
+    }
+}
---
+++ OOP/notebook/src/main/java/ru/otus/solid/Persister.java
@@ -0,0 +1,13 @@
+package ru.otus.solid;
+
+public class Persister {
+    private final User user;
+
+    public Persister(User user) {
+        this.user = user;
+    }
+
+    public void save() {
+        System.out.println("Save user: " + user.getName());
+    }
+}
